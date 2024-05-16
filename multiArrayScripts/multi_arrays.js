0bd0a649ac1533ec00c5b15f97e83cc1b94a3857
Antonio <asoriano@appdev.yearup.org>
2024-05-16 13:54:52 -0500

    completed multi_arrays.js

---
+++ multiArrayScripts/multi_arrays.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let lockerAndAccessCode = [
+    ["Locker 1", 135],
+    ["Locker 2", 159],
+    ["Locker 3", 642]
+];
+
+// prints 642
+console.log(lockerAndAccessCode[2][1]);
+
+// prints ["Locker 2", 159]
+console.log(lockerAndAccessCode[1]);
